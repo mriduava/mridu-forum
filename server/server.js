ff7d7b574bb0e8c01d02867cf6be73493ca939ba
mriduava <mriduava@gmail.com>
2020-12-19 15:12:55 +0100

    configure passport & used to autheticate user by login

--- server/server.js
+++ server/server.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser= require('body-parser')
+const bodyParser= require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const Forum = require('./models/forum');
 const User = require('./models/user');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-
+// CONNECT MONGODB
 const db = "forumdb";
 mongoose.connect('mongodb://localhost:27017/' + db, { 
   useUnifiedTopology: true,
   useNewUrlParser: true 
 });
 
+// BODY-PARSER CONFIG
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// PASSPORT CONFIG
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// GET ALL FORUM POSTS
 app.get('/', async (req, res) => {
   await Forum.find()
     .then(allPosts => res.json(allPosts))
 })
 
+// POST A NEW FORUM
 app.post('/', async (req, res) => {
   await Forum.create(req.body)
     .then(newPost => res.json(newPost))
 })
 
+// GET ALL USERS
 app.get('/users', async (req, res) => {
   await User.find({}, 'username', (err, users)=>{
       res.json(users);
@@ -31,7 +45,7 @@ app.get('/users', async (req, res) => {
 })
 
 // REGISTER USER
-app.post('/users', async (req, res) => {
+app.post('/users/register', async (req, res) => {
   let newUser= new User({
       username: req.body.username,
       role: req.body.role}),
@@ -45,5 +59,13 @@ app.post('/users', async (req, res) => {
   }); 
 });
 
+// SIGN IN USER
+app.post('/users/signin', passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/users/signin'
+  })
+);
+
+// SERVER
 const PORT = process.env.PORT || 3200;
 app.listen(PORT, console.log(`SERVER IS RUNNING AT PORT ${PORT}`));
