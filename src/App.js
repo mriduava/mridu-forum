b35b9df7fefd5b5b661ec564067ae4cdd0d4fb78
mriduava <mriduava@gmail.com>
2021-01-04 12:16:48 +0100

    add initial home & about pages in frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
   return (
     <div className="App">
-      <h1>MRIDU FORUM</h1>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Router>
     </div>
   );
 }
