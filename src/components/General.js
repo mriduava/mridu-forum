f8eeab47cd77bc0119a175a323c61c60c753cac2
mriduava <mriduava@gmail.com>
2021-01-08 19:37:41 +0100

    create writing form and used in general mypage

--- src/components/General.js
+++ src/components/General.js
@@ -1,16 +1,29 @@
-import React, {useContext} from 'react'
+import React, {useContext } from 'react'
 import { Container, Row, Col } from 'reactstrap';
 import { UserContext } from '../contexts/UserContextProvider'
+import WritingForm from './WritingForm'
 
 const General = () => {
   const { user } = useContext(UserContext)
+
   return (
     <Container className="border border-warning pb-5" style={{minHeight:"80vh"}}>
-      <h4 className="text-dark font-weight-bold pt-5">GENERAL</h4>
-      <hr/>
       <Row className="d-flex flex-column">
         <Col xs="12" sm="8" md="6" lg="5" className="pl-3 pt-0"> 
           <h1>{user&&user.username}</h1>
+          <h5 className="text-info font-weight-bold pt-5 ml-3">GENERAL</h5>
+        </Col>
+      </Row>
+      <hr/>
+      <Row className="mx-auto">
+        <Col xs="12" sm="12" md="6" lg="6">
+          <h4 className="ml-0">Create a thread</h4>
+          <hr className="ml-1"/>
+          <WritingForm/>
+        </Col>
+         <Col xs="12" sm="12" md="6" lg="6">
+           <h4 className="ml-3">My threads</h4>
+           <hr className="ml-3"/>
         </Col>
       </Row>
     </Container>
---
+++ src/components/WritingForm.js
@@ -0,0 +1,37 @@
+import React, {useContext} from 'react'
+import { ForumContext } from '../contexts/ForumContextProvider'
+import { Form, FormGroup, Input } from 'reactstrap';
+
+const WritingForm = () => {
+  const { subjects } = useContext(ForumContext)
+
+  const mapSubjects = () => {
+    return subjects.map((subject, i) => {
+      return (
+        <option key={'sub' + subject._id + i}>{subject.subject}</option>
+      )
+    })
+  }
+
+  return ( 
+    <Form className="mt-3">
+      <FormGroup>
+        <p className="py-0 m-0 text-secondary" style={{fontSize: "14px"}}>Select a sub-forum</p>
+        <Input type="select" name="select" id="exampleSelect" required>
+          {subjects&&mapSubjects()}
+        </Input>
+      </FormGroup>
+      <FormGroup>
+        <Input type="text" name="topic" id="topic" placeholder="Write a title..."  required/>
+      </FormGroup>     
+      <FormGroup>
+        <Input type="textarea" name="text" id="text" placeholder="Write in detail..." required/>
+      </FormGroup>
+      <button className="btn btn-outline-success mt-0" 
+        style={{height: "26px", width: "120px", fontSize: "13px", paddingTop: "2px"}}>Submit
+      </button>
+    </Form>
+  )
+}
+
+export default WritingForm
