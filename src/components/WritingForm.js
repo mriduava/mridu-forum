2577a88e01507d0cc6ecbd9a2f11a29bcc9057e0
mriduava <mriduava@gmail.com>
2021-01-09 21:31:55 +0100

    add create-thread func in general-page to post thread

--- src/components/WritingForm.js
+++ src/components/WritingForm.js
@@ -1,40 +1,93 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useState, useEffect } from 'react'
 import { ForumContext } from '../contexts/ForumContextProvider'
 import { Form, FormGroup, Input } from 'reactstrap';
 
 const WritingForm = () => {
   const { subjects } = useContext(ForumContext)
+  const [subforumId, setSubforumId] = useState('')
+  const [topic, setTopic] = useState('')
+  const [text, setText] = useState('')
+  const [message, setMessage] = useState('')
 
   const mapSubjects = () => {
-    return subjects.map((subject, i) => {
-      return (
-        <option key={'sub' + subject._id + i}>{subject.subject}</option>
-      )
-    })
+    return(
+      <Input type="select" name="select" id="exampleSelect" required
+        onChange={e=>setSubforumId(e.target.value)}>
+         <option value =''>Select a Sub-Forum</option>
+         {(() => {
+          return subjects.map((subject, i) => {
+            return (
+              <option key={'sub' + subject._id + i} 
+                value={subject._id}>
+                {subject.subject}
+              </option>
+            )
+          })
+        })()}
+      </Input>
+    )
   }
 
-  useEffect(()=>{
-    mapSubjects()
-  }, [])
+  const displayAlert = () => {
+    return(
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">{message}</div>
+    )
+  }
 
-  return ( 
-    <Form className="mt-3 mb-4">
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (message!==null) {
+        setMessage(null)
+      }
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [message]);
+
+  const createThread = async (e) => {
+    e.preventDefault();
+    await fetch(`/api/forums/${subforumId}`, {
+      method: 'POST',
+      body: JSON.stringify({topic, text}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then((response) => {
+      if (response.ok) {
+        setMessage("Thread submission successful!") 
+        setSubforumId('')
+        setTopic('')
+        setText('')      
+      } else {
+        setMessage("Thread submission failed!")
+      }
+    })
+    .catch((error) => {
+      return Promise.reject();
+    });
+  }
+
+  return (
+    <>
+    <Form className="mt-3 mb-4" onSubmit={createThread}>
       <FormGroup>
-        <p className="py-0 m-0 text-secondary" style={{fontSize: "14px"}}>Select a sub-forum</p>
-        <Input type="select" name="select" id="exampleSelect" required>
-          {subjects&&mapSubjects()}
-        </Input>
+        {subjects&&mapSubjects()}   
       </FormGroup>
       <FormGroup>
-        <Input type="text" name="topic" id="topic" placeholder="Write a title..."  required/>
+        <Input type="text" name="topic" id="topic" placeholder="Write a title..." 
+          value={topic} onChange={e=>setTopic(e.target.value)} required/>
       </FormGroup>     
       <FormGroup>
-        <Input type="textarea" name="text" id="text" placeholder="Write in detail..." required/>
+        <Input type="textarea" name="text" id="text" placeholder="Write in detail..." 
+          value={text} onChange={e=>setText(e.target.value)} required/>
       </FormGroup>
       <button className="btn btn-outline-success mt-0" 
         style={{height: "26px", width: "120px", fontSize: "13px", paddingTop: "2px"}}>Submit
       </button>
     </Form>
+      {message&&displayAlert()}
+    </>
   )
 }
 
